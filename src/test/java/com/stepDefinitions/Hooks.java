9d6581262745b730468e00b18ab40806609b57a8
RGallardo <ragg86@gmail.com>
2021-01-04 23:08:42 -0500

    added hook class

---
+++ src/test/java/com/stepDefinitions/Hooks.java
@@ -0,0 +1,21 @@
+package com.stepDefinitions;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+
+public class Hooks {
+
+
+    @Before
+    public void setUp(){
+
+
+    }
+
+    @After
+    public void tearDown(){
+
+
+
+    }
+}
