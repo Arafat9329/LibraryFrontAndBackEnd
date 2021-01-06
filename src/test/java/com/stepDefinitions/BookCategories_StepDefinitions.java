a258c291f8d4ce7a11af6f20a9c7c1ee88593265
RGallardo <ragg86@gmail.com>
2021-01-05 19:46:35 -0500

    added BookCategories_StepDefinitions

---
+++ src/test/java/com/stepDefinitions/BookCategories_StepDefinitions.java
@@ -0,0 +1,22 @@
+package com.stepDefinitions;
+
+import com.pages.BooksPage;
+import io.cucumber.java.en.Then;
+
+public class BookCategories_StepDefinitions {
+
+    private BooksPage booksPage = new BooksPage();
+
+
+    @Then("user select {string} from Book Category")
+    public void user_select_from_book_category(String category) {
+        booksPage.selectCategory(category);
+    }
+
+    @Then("The table should display only {string} books")
+    public void the_table_should_display_only_books(String category) {
+        booksPage.booksDisplayCategoryIsCorrect(category);
+
+    }
+
+}
