466dbda146db709c7f1e2dce45ca2fa5002d9313
Dawud_SF <dawudabduweli@gmail.com>
2021-01-06 19:59:15 -0800

    added failed runner

--- src/test/java/com/runners/CucumberRunner.java
+++ src/test/java/com/runners/CucumberRunner.java
@@ -7,6 +7,10 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
+        plugin = {
+                "rerun:target/rerun.txt",
+                "json:target/cucumber.json"
+        },
         features = "src/test/resources/features",
         glue = "com/stepDefinitions",
         dryRun = false,
---
+++ src/test/java/com/runners/FailedRunner.java
@@ -0,0 +1,13 @@
+package com.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        glue = "com/vytrack/step_definitions",
+        features = "@target/rerun.txt"
+)
+public class FailedRunner {
+}
