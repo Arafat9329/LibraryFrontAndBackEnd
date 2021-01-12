74e8054520c577bda38f7a9bdf821536e2a27309
RGallardo <ragg86@gmail.com>
2021-01-11 21:32:27 -0500

    added libraryUtils to get token

---
+++ src/test/java/com/utils/LibraryUtils.java
@@ -0,0 +1,52 @@
+package com.utils;
+
+
+import io.restassured.http.ContentType;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+public class LibraryUtils {
+
+
+    public static String getTokenBy_Env(String environment, String email, String password){
+        return   given()
+                .log().all()
+                .contentType(ContentType.URLENC)
+                .formParam("email" , ConfigurationReader.getProperty(email))
+                .formParam("password", ConfigurationReader.getProperty(password)).
+        when()
+                .post(environment+"/login").
+        then()
+                .extract()
+                .jsonPath().getString("Token");
+    }
+
+
+    public static String getTokenDefault_Env(){
+        return
+                given()
+                        //.log().all()
+                        .contentType( ContentType.URLENC  )
+                        .formParam("email", ConfigurationReader.getProperty("librarian2.Username"))
+                        .formParam("password",ConfigurationReader.getProperty("librarian2.Password")).
+                        when()
+                        .post("http://library2.cybertekschool.com/rest/v1/login").
+                        then()
+                        //.log().all()
+                        .assertThat()
+                        .statusCode( is(200))
+                        .contentType(ContentType.JSON)
+                        .body("token", is( not( emptyString() ) )  )
+                        .extract()
+                        .jsonPath()
+                        .getString("token");
+
+    }
+
+
+
+
+
+
+}
